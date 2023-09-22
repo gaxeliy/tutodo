bd3448e50f9f2d4b7210ffeb00e739fd4f47de4b
gaxeliy <gaxeliy@gmail.com>
2023-09-22 22:03:30 +0500

    Fix tests and mock

--- app/tests/test_tasks.py
+++ app/tests/test_tasks.py
@@ -2,6 +2,8 @@ import asyncio
 from unittest.mock import patch
 
 import pytest
+from httpx import AsyncClient
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 from starlette.testclient import TestClient
@@ -9,6 +11,7 @@ from testcontainers.postgres import PostgresContainer
 
 from app import dependencies
 from app.database import Base
+from app.dependencies import get_repository
 from app.main import app
 from app.tasks.use_cases import Repository
 
@@ -16,11 +19,14 @@ from app.tasks.use_cases import Repository
 @pytest.fixture
 def get_repository_():
     postgres = PostgresContainer(
-        'postgres:14.9-alpine3.18',
+        'postgres:14.9-alpine3.18', user='postgres', password='postgres', dbname='postgres', port=5432
     )
     with postgres:
         postgres.driver = "asyncpg"
-        engine = create_async_engine(postgres.get_connection_url(), echo=True, future=True)
+        postgres.get_container_host_ip = lambda: "0.0.0.0"
+        # postgres.port_to_expose = 5432
+        connection_url = postgres.get_connection_url()
+        engine = create_async_engine(connection_url, echo=True, future=True, poolclass=NullPool)
         async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)  # type: ignore
         rep = Repository(async_session)
 
@@ -30,20 +36,20 @@ def get_repository_():
                 await conn.run_sync(Base.metadata.create_all)
         asyncio.run(init_models())
 
-        with patch.object(dependencies, 'get_repository', return_value=rep):
-            yield
+        yield rep
 
 
 @pytest.fixture
 def app_client(get_repository_):
-    return TestClient(app)
+    app.dependency_overrides[get_repository] = lambda: get_repository_
+    return AsyncClient(app=app, base_url='http://localhost:8000/')
 
 
 @pytest.mark.asyncio
 async def test_get_tasks(app_client):
-    result = app_client.post('/tasks', json={'title': 'test',
+    result = await app_client.post('/tasks/', json={'title': 'test',
                                              'description': 'test',
                                              'done': False,
                                              'scheduled_at': None,
                                              'my_day_date': None})
-    pass
+    assert result.status_code == 200
